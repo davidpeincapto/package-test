bf68e9ef9db12123e0b28c78971e6b76207c1c0f
David Pegueroles <david.pegueroles@incapto.com>
2024-05-22 17:54:24 +0200

    Adapts vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"), // Asegúrate de que la ruta de entrada sea correcta
+      name: "MyComponentLibrary",
+      fileName: (format) => `my-component-library.${format}.js`,
+    },
+    rollupOptions: {
+      // Asegúrate de externalizar dependencias que no deben incluirse en el paquete
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
