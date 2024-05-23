de234e145398ad7a8135b08290dbfccc39176532
David Pegueroles <david.pegueroles@incapto.com>
2024-05-23 17:27:39 +0200

    Canvis 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.tsx"), // Asegúrate de que la ruta de entrada sea correcta
-      name: "MyComponentLibrary",
-      fileName: (format) => `my-component-library.${format}.js`,
+      entry: path.resolve(__dirname, "src/index.ts"), // Asegúrate de que la ruta de entrada sea correcta
+      name: "davidpeincapto",
+      fileName: (format) => `davidpeincapto.${format}.js`,
     },
     rollupOptions: {
       // Asegúrate de externalizar dependencias que no deben incluirse en el paquete
